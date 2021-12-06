a9ee24514754b04f232837858917067782eee354
minjoon_lee <minjoon1995@naver.com>
2021-12-06 17:44:54 +0900

    feature : user entity 및 userRepo 생성

--- src/main/java/com/zipbom/zipbom/Auth/repository/UserRepository.java
+++ src/main/java/com/zipbom/zipbom/Auth/repository/UserRepository.java
@@ -2,6 +2,13 @@ package com.zipbom.zipbom.Auth.repository;
 
 import com.zipbom.zipbom.Auth.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+    @Query(value = "select * from user where user_id = :id",nativeQuery = true)
+    Optional<User> findByUserId(@Param("id") String id);
 }
