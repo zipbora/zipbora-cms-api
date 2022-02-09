4ea96865b8ae235b76eb601f78a3464f2b493f64
mj950425 <minjoon1995@naver.com>
2022-02-09 20:06:37 +0900

    add : jwt annotation, AdminAuthority enum 추가

---
+++ src/main/java/com/zipbom/zipbom/Auth/jwt/AdminAuthority.java
@@ -0,0 +1,8 @@
+package com.zipbom.zipbom.Auth.jwt;
+
+public enum AdminAuthority {
+    ROLE_SUPER_ADMIN,
+    ROLE_SUB_MASTER,
+    ROLE_SUB,
+    ROLE_OPERATOR
+}
---
+++ src/main/java/com/zipbom/zipbom/global/JwtAuthorityChecker.java
@@ -0,0 +1,14 @@
+package com.zipbom.zipbom.global;
+
+import com.zipbom.zipbom.Auth.jwt.AdminAuthority;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface JwtAuthorityChecker {
+    AdminAuthority authority() default AdminAuthority.ROLE_OPERATOR;
+}
