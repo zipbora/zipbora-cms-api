c44ebac3be05e9f78d27470f4999972b84610d61
minjoon_lee <minjoon1995@naver.com>
2022-02-10 11:24:27 +0900

    fix : 안쓰는 라이브러리 제거

--- src/main/java/com/zipbom/zipbom/Auth/controller/AuthController.java
+++ src/main/java/com/zipbom/zipbom/Auth/controller/AuthController.java
@@ -11,7 +11,6 @@ import com.zipbom.zipbom.Util.dto.CMRespDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
