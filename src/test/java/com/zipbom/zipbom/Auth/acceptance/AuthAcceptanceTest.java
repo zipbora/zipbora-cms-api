bc51e556dfa82fb230b7d98be8433a7e95cac5b2
minjoon_lee <minjoon1995@naver.com>
2022-02-13 21:47:30 +0900

    add:  test코드 데이터베이스 클린업 추가

---
+++ src/test/java/com/zipbom/zipbom/Auth/acceptance/AuthAcceptanceTest.java
@@ -0,0 +1,32 @@
+package com.zipbom.zipbom.Auth.acceptance;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AuthAcceptanceTest extends AcceptanceTest{
+
+    @Test
+    void loginTest() {
+
+    }
+
+    public static ExtractableResponse<Response> 회원_생성_요청(String email, String password, Integer age) {
+        Map<String, String> params = new HashMap<>();
+        params.put("email", email);
+        params.put("password", password);
+        params.put("age", age + "");
+
+        return RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(params)
+                .when().post("/members")
+                .then().log().all().extract();
+    }
+}
