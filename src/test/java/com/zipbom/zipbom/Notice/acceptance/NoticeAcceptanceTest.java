a6fc9e3d4d44238df39fa414247c542fea6c2a5c
MinJoonLee <52944973+mj950425@users.noreply.github.com>
2022-02-14 23:25:01 +0900

    add : notice test code 생섯ㅇ

---
+++ src/test/java/com/zipbom/zipbom/Notice/acceptance/NoticeAcceptanceTest.java
@@ -0,0 +1,38 @@
+package com.zipbom.zipbom.Notice.acceptance;
+
+import com.zipbom.zipbom.CustomerSupport.model.Notice;
+import com.zipbom.zipbom.Util.AcceptanceTest;
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class NoticeAcceptanceTest extends AcceptanceTest {
+
+    @Test
+    @DisplayName("공지사항을 만든다")
+    void createNotice() {
+        ExtractableResponse<Response> response = 공지사항_생성_요청("제목입니당", "내용입니당");
+        assertThat(response.jsonPath().getString("title")).isEqualTo("제목입니당");
+    }
+
+    private ExtractableResponse<Response> 공지사항_생성_요청(String title, String content) {
+        Map<String, String> params = new HashMap<>();
+        params.put("title", title);
+        params.put("content", content);
+        return RestAssured
+                .given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/notice")
+                .then().log().all().extract();
+    }
+}
