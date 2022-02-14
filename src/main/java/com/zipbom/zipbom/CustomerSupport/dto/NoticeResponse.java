ada2d32accd4f3dbd4dffcf3c191b67c8c3e9d0e
MinJoonLee <52944973+mj950425@users.noreply.github.com>
2022-02-14 23:52:51 +0900

    fix : NoticeResponse 에 getter를 안붙여주면 406이 뜬다

--- src/main/java/com/zipbom/zipbom/CustomerSupport/dto/NoticeResponse.java
+++ src/main/java/com/zipbom/zipbom/CustomerSupport/dto/NoticeResponse.java
@@ -9,9 +9,24 @@ public class NoticeResponse {
 
     private String content;
 
+    protected NoticeResponse() {
+    }
+
     public NoticeResponse(Notice notice) {
         this.id = notice.getId();
         this.title = notice.getTitle();
         this.content = notice.getContent();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Long getId() {
+        return id;
     }
 }
