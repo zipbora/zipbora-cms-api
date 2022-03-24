1707371de95c7d585d5e424144b4732f13613f61
minjoon_lee <minjoon1995@naver.com>
2022-03-24 19:57:08 +0900

    fix : signupdto 수정

--- src/main/java/com/zipbom/zipbom/Auth/dto/SignUpRequestDto.java
+++ src/main/java/com/zipbom/zipbom/Auth/dto/SignUpRequestDto.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 
 @Getter
 public class SignUpRequestDto {
-    private String id;
     private String email;
     private String nickname;
+    private String imageEncoding;
 }
